de1ba4d5986bb4facc94151a54d3a0252c92959d
Marcosac1 <marcosaccone8@gmai.com>
2026-04-30 11:18:49 +0200

    Refactor Stack class: fix attribute naming, enhance method documentation, and improve usage examples

--- 03_Stack_pila.py
+++ 03_Stack_pila.py
@@ -1,27 +1,38 @@
 class Stack:
-    
-    def __init__(self):
-        self.item = [] #inizializo una pila vuota
+    def __init__(self):# Inizializza una pila vuota.
+        self.__items = []
 
-    def is_empty(self): #verifica se la pila e vuota
-        return len(self.items) == 0
-    
-    def push(self, item): #aggiunge un elemento in cima alla pila.
-        self.item.append(item)
-    
-    def pop(self): # rimuve e restituisce l'elemento in cima solleva un errore se vuota    
+    def is_empty(self):# Verifica se la pila è vuota.
+        return len(self.__items) == 0
+
+    def push(self, __item):# Aggiunge un elemento in cima alla pila.
+        self.__items.append(__item)
+
+    def pop(self):# Rimuove e restituisce l'elemento in cima. Solleva un errore se vuota.
         if not self.is_empty():
-            return self.items.pop()
-        raise IndexError(f"pop da una pila vuota")
+            return self.__items.pop()
+        raise IndexError("pop da una pila vuota")
+
+    def peek(self):# Restituisce l'elemento in cima senza rimuoverlo.
+        if not self.is_empty():
+            return self.__items[-1]
+        return None
+
+    def size(self):# Restituisce il numero di elementi nella pila.
+        return len(self.__items)
+
+    def insert(self, index, item):
+        self.__items.insert(index, item)
+
+    def __str__(self):# Permette di stampare la pila.
+        return str(self.__items)
     
-    def peek(self): #permette di stampare la pila
-        return str(self.item)
-        
+
 # --- Esempio di Utilizzo ---
 pila = Stack()
-pila.push('A')
-pila.push('B')
-pila.push('C')
+pila.push('Google.com') # A
+pila.push('Wikipedia.com') # B
+pila.push('python.org') # C
 
 print(f"Pila attuale: {pila}")      # Output: ['A', 'B', 'C']
 print(f"Elemento in cima (peek): {pila.peek()}") # Output: C
@@ -29,4 +40,6 @@ print(f"Pop: {pila.pop()}")         # Output: C
 print(f"Pila dopo pop: {pila}")     # Output: ['A', 'B']
 print(f"Dimensione: {pila.size()}") # Output: 2
 
+pila.insert(1, 'www.proton.me')
 
+print(f"Pila dopo l'insert: {pila}")
